6eb61dd4d61fa0a83a5e5a7e4c9100252b601efd
DjonysDalmy <Djonysdalmy@gmail.com>
2022-05-24 21:50:10 -0300

    update page

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   M.Sidenav.init(document.querySelectorAll(".sidenav"));
   M.FloatingActionButton.init(document.querySelectorAll(".fixed-action-btn"));
+  M.Tabs.init(document.querySelectorAll(".tabs"));
 });
